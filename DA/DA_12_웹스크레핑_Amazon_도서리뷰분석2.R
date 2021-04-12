76f0e914373c43730177b1e6cff3ccc1a3b14b1d
hongsun2da <hongsun2da@gmail.com>
2021-04-12 21:24:12 +0900

    파일 추가

---
+++ DA/DA_12_웹스크레핑_Amazon_도서리뷰분석2.R
@@ -0,0 +1,10 @@
+
+load("amazon_review_cheese01.rda")
+ls()
+
+str(amazon_review) # 'data.frame':	3670 obs. of  8 variables:
+
+View(amazon_review)
+
+install.packages("tidyverse")
+library(tidyverse)
